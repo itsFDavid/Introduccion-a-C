961de0de9b34ec54bce318265dcc4fc85383bc73
itsFDavid <FDavid04@icloud.com>
2024-02-21 09:21:07 -0600

    upload homeWork

--- IntroduccioToC/clase3/tarea.c
+++ IntroduccioToC/clase3/tarea.c
@@ -13,3 +13,19 @@ b) 5 + 2/1
        ///
         2-/4
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float resultA, resultB;
+
+    resultA= ((1.0/3.0)+(3.0/5.0)+(1.0/30.0))/(23.0/30.0);
+    resultB= (5.0+(2.0/(1.0/(1.0+(2.0/(2.0-(1.0/4.0)))))));
+
+
+    printf("%f\n", resultA);
+    printf("%f\n", resultB);
+    
+    return 0;
+}
