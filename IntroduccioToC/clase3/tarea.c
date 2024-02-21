f868bb3983a619e1159d486110f42624ca98429a
itsFDavid <FDavid04@icloud.com>
2024-02-21 08:38:44 -0600

    update clase3

---
+++ IntroduccioToC/clase3/tarea.c
@@ -0,0 +1,15 @@
+/*
+Hacer un programa en Lenguaje c que haga los siguientes calculos y 
+muestre los resultados en pantalla.
+
+
+a)  1/3 + 3/5 + 1/30            = 0.9768
+    ////////////////            
+        23/30
+    
+b) 5 + 2/1
+    //////
+    1+ 2/1
+       ///
+        2-/4
+*/
