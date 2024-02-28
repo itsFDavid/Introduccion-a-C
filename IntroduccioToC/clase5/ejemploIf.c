f97cd0cf6414cfcf050a6a54571c9d9244947979
itsFDavid <FDavid04@icloud.com>
2024-02-28 07:42:32 -0600

    create clase5

---
+++ IntroduccioToC/clase5/ejemploIf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero = 98;
+    if(numero < 50){
+        printf("EL numero es menor que 50\n");
+    }else{
+        printf("El numero es mayor de 50\n");
+    }
+    printf("Fin del Programa\n");
+
+    return 0;
+}
---
+++ IntroduccioToC/clase5/ifMayor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero= 35;
+    if(numero < 100){
+        printf("El numero es menor que 100 \n");
+        if(numero > 50){
+            printf("El numero es mayor que 50 \n");
+        }
+    }
+    printf("Fin del programa\n");
+    return 0;
+}
