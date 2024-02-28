d555ecb59a440cec85069b423e3998cc284cdc1b
itsFDavid <FDavid04@icloud.com>
2024-02-27 18:39:51 -0600

    create saltos.c

---
+++ IntroduccioToC/Programas_intro/saltos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    
+    printf("Este fue un texto \n");
+    printf("que fue impreso desde c");
+    printf("\n");
+    printf("Ya soy todo un programador");
+    printf("\n");
+
+    return 0;
+
+
+}
