5689209e8003d10bb3d8d9495bc7b4941420475f
itsFDavid <FDavid04@icloud.com>
2024-02-28 08:38:58 -0600

    Update clase3 and clase5

--- IntroduccioToC/clase5/while.c
+++ IntroduccioToC/clase5/while.c
@@ -2,12 +2,27 @@
 
 int main(){
 
-    int contador=0;
-    while( contador < 3){
-        printf("Hola\n");
+    int contador=1;
+    while( contador <= 1000){
+        printf("%d, ", contador);
         contador++;
     }
     printf("Fin\n");
 
     return 0;
 }
+
+/*
+int main(){
+
+    int contador=1;
+    while( contador <= 3){
+        printf("hola\n");
+        contador++;
+    }
+    printf("Fin\n");
+
+    return 0;
+}
+
+*/
