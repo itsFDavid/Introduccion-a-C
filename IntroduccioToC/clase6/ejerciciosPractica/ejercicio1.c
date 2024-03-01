6186195fdb90faa230a7f065f62ae1695f616816
itsFDavid <FDavid04@icloud.com>
2024-02-29 20:37:31 -0600

    Update file

--- IntroduccioToC/clase6/ejerciciosPractica/ejercicio1.c
+++ IntroduccioToC/clase6/ejerciciosPractica/ejercicio1.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-void numeroMultiplo(int numero, int rango, int lista[], int *contador) {
+void numeroMultiplo(int numero, int numero2, int numero3, int rango, int lista[], 
+                    int lista2[], int *contador) {
     int n = 1;
     *contador = 0; 
 
     while (n <= rango) {
-        if (n % numero == 0) {
+        if (n % numero == 0 && n% numero2) {
             lista[*contador] = n;
             (*contador)++;
+            if (n==numero3){
+                lista2[*contador]= n*n;
+                lista2[*contador+1]=n*n*n;
+                (*contador)++;
+            }
+            
         }
         n++;
     }
@@ -18,7 +25,7 @@ void numeroMultiplo(int numero, int rango, int lista[], int *contador) {
 int main(){
     int numero, numero2, numero3;
     int rango=0, contador=0;
-    int lista[1000];
+    int lista[1000], lista2[1000];
     printf("Ingresa un numero entero para obtener los multiplos: ");
     scanf("%d", &numero);
     printf("Ingresa un numero entero para omitir los multiplos de este: ");
@@ -29,19 +36,22 @@ int main(){
     scanf("%d", &rango);
 
     if(numero2==numero){
-        puts("El numero 1 y 2 son el mismo");
+        puts("El numero 1 y 2 son el mismo\n");
         return 0;
     }
     if (rango==0){
-        puts("El rango no es aceptable");
+        puts("El rango no es aceptable\n");
+        return 0;
     }
-    numeroMultiplo(numero, rango, lista, &contador);
+    numeroMultiplo(numero, numero2, numero3, rango, lista, lista2, &contador);
 
-    printf("Los multiplos de %d dentro del rango son: \n", numero);
+    printf("Los resultados son: \n");
 
     for (int i = 0; i < contador; i++) {
         printf("%d, ", lista[i]);
     }
+    
+    
     
     return 0;
 }
