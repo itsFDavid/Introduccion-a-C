f0527291cbecd03c24a7e4ff3cd717f6f0b985b4
itsFDavid <fdavid04@icloud.com>
2024-03-04 21:14:14 -0600

    update ejercicio 2

--- IntroduccioToC/clase6/ejerciciosPractica/ejercicio2.c
+++ IntroduccioToC/clase6/ejerciciosPractica/ejercicio2.c
@@ -3,12 +3,15 @@ void limpiar_terminal() {
     printf("\033[2J"); 
     printf("\033[H");
 }
-
+int factorial(int numero){
+    if(numero <= 1){ return 1;}
+    else{ return numero*factorial(numero-1);}
+}
 int main(){
     int opcion, num1, num2;
 
 
-    while (opcion==0){
+    do{
         puts("Escoje la operacion a realizar\n");
         puts("[1] Suma");
         puts("[2] Resta");
@@ -24,21 +27,45 @@ int main(){
                 puts("Ingresa numero a sumar: ");
                 scanf("%d", &num1);
                 puts("Ingresa otro numero: ");
-                scanf("%d", num2);
-                printf("El resultado de sumar %d + %d : %d\n", num1, num2, (num1+num2));
+                scanf("%d", &num2);
+                printf("El resultado de sumar %d + %d: %d\n", num1, num2, (num1+num2));
                 break;
             case 2:
                 puts("Ingresa numero a restar: ");
                 scanf("%d", &num1);
                 puts("Ingresa otro numero: ");
-                scanf("%d", num2);
-                printf("El resultado de restar %d + %d : %d\n", num1, num2, (num1-num2));
+                scanf("%d", &num2);
+                printf("El resultado de restar %d - %d: %d\n", num1, num2, (num1-num2));
                 break;
+            case 3:
+                puts("Ingresa numero a multiplicar: ");
+                scanf("%d", &num1);
+                puts("Ingresa otro numero: ");
+                scanf("%d", &num2);
+                printf("El resultado de multiplicar %d * %d: %d\n", num1, num2, (num1*num2));
+                break;
+            case 4:
+                puts("Ingresa numero a dividir: ");
+                scanf("%d", &num1);
+                puts("Ingresa otro numero: ");
+                scanf("%d", &num2);
+                printf("El resultado de dividir %d / %d: %d\n", num1, num2, (num1/num2));
+                break;
+            case 5:
+                puts("Ingresa numero a obtener el factorial: ");
+                scanf("%d", &num1);
+                printf("El resultado de el factorial de %d: %d\n", num1, factorial(num1));
+                break;
+
         
         default:
+            puts("Opcion invalida, por favor ingrese una opcion dentro del rango");
             break;
+        
         }
-    }
+    }while(opcion==0);
+        puts("Fin de programa");
+    
     
 
 
