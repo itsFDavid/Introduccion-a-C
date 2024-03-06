1722258ba0326d1d09bfecaf3dea39e62be7b1b7
Francisco David <fdavid04@icloud.com>
2024-03-06 08:53:32 -0600

    Delete IntroduccioToC/clase2 directory

--- IntroduccioToC/clase2/constantes.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define PI 3.1415926
-
-int main(){
-    printf("PI es igual a = %f \n", PI);
-    return 0;
-}
--- IntroduccioToC/clase2/operacionesAritmeticas.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("La suma de %d + %d es igual a = %d\n", 5, 8, 5+8);
-
-    printf("La suma de %d + %d es igual a = %d\n", 78787, 3259, 78787+3259);
-    return 0;
-}
