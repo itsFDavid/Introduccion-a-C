d9a90442299bdc5334203cd183faa5de6feb0604
itsFDavid <fdavid04@icloud.com>
2024-03-13 22:40:33 -0600

    Update clase2-marzo

---
+++ IntroduccioToC/marzo/clase2/p2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int vector[100];
+    int i=99;
+    int p=0;
+    while(i>=0){
+        printf("Vector[%d]: %d \n", p, vector[i]= i);
+        i--;
+        p++;
+    }
+
+    return 0;
+}
