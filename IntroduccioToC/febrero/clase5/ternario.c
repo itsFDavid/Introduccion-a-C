ca82f69f402a0462833d1f4c57948a01f24b68f6
itsFDavid <fdavid04@icloud.com>
2024-03-23 18:03:39 -0600

    Update files

--- IntroduccioToC/febrero/clase5/ternario.c
+++ IntroduccioToC/febrero/clase5/ternario.c
@@ -6,9 +6,9 @@ int main(){
     int descuentoCliente= 20;
     int cliente= 1; 
     int z= 0;
-
+    int cantidad= 1300;
     z= (cliente) ? descuentoCliente : descuento; // solo aplica a booleanos
     printf("El descuento es de %d\n", z);
-
+    printf("El total es: %d",cantidad-((z*cantidad)/100));
     return 0;
 }
