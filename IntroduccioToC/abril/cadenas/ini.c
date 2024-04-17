c4fc1081c3fc5715b6e029d58576e156fd8d9e45
itsFDavid <fdavid04@icloud.com>
2024-04-17 14:57:09 -0600

    add examples to intro to cadenas

---
+++ IntroduccioToC/abril/cadenas/ini.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char c[10];
+
+
+    //int i;
+    printf("Introduce una palabra\n");
+    scanf("%s", c);
+    printf("%s\n", c);
+
+
+
+    return 0;
+}
+
+
+
+/*
+
+    '\0 ò \o' es un espacio en blanco y siempre las cadenas debe acabr con esto
+
+
+*/
---
+++ IntroduccioToC/abril/cadenas/ini2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(){
+    char cad[]= "Es una cadena";
+
+    printf("%s\n", cad);
+
+    return 0;
+}
