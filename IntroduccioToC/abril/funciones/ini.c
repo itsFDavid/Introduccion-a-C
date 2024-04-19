5a88b9b6061abc5c4c814ecbabcce761b3377c41
itsFDavid <fdavid04@icloud.com>
2024-04-18 21:22:25 -0600

    add intro to funciones

---
+++ IntroduccioToC/abril/funciones/ini.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+float promedio(float a, float b);
+
+int main(){
+    float prom, a=5, b=10;
+    prom= promedio(a, b);
+    printf("EL promedio es: %.2f\n", prom);
+    return 0;
+}
+
+
+float promedio(float a, float b){
+    float prom=0;
+
+    a=a+3;
+    b=b+3;
+    prom= (a+b)/2;
+    return prom;
+}
