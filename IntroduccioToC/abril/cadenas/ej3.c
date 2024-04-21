8f79c788f4e5b0a39f626f16115e83c46ef6d874
itsFDavid <fdavid04@icloud.com>
2024-04-20 21:43:30 -0600

    add excersice 3 to cadenas

--- IntroduccioToC/abril/cadenas/ej3.c
+++ IntroduccioToC/abril/cadenas/ej3.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+int main(){
+    char pal1[50], pal2[50];
+    int len=0;
+    puts("Ingresa la pimer palabra: ");
+    scanf("%s", pal1);
+    puts("Ingresa la segunda palabra: ");
+    scanf("%s", pal2);
+    len = strlen(pal1);
+    printf("Las veces que se imprimira %s es: %d\n", pal2, len);
+    printf("%s", pal1);
+    for(int i=0; i<len;i++){
+        printf("%s", pal2);
+    }
+    printf("\n");
+
+
+    return 0;
+}
