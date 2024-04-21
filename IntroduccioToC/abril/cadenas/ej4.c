ef46e56a3738c6100fb03f694fd8edcd2ffee55c
itsFDavid <fdavid04@icloud.com>
2024-04-20 21:43:42 -0600

    add excersice 4 to cadenas

--- IntroduccioToC/abril/cadenas/ej4.c
+++ IntroduccioToC/abril/cadenas/ej4.c
@@ -1,2 +1,19 @@
-//tarea
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char pal1[50], pal2[50];
+    int res;
+    puts("Ingresa la primer palabra: ");
+    scanf("%s", pal1);
+    puts("Ingresa la primer palabra: ");
+    scanf("%s", pal2);
+    res = strcmp(pal1, pal2);
+    printf("%d\n", res);
+    if(res==0)puts("Ambas palabras son iguales");
+    if(res>0)printf("La palabra ´%s´ es mas grande que ´%s´\n", pal2, pal1);
+    if(res<0)printf("La palabra ´%s´ es mas grande que ´%s´\n", pal1, pal2);
+
+    return 0;
+}
 
