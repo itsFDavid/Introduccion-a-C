c5b23c0299558fbd419e4b92c02367b495eb0cad
itsFDavid <fdavid04@icloud.com>
2024-05-07 20:54:13 -0600

    refactor: Swap the values of two strings in ej2.c

--- IntroduccioToC/abril/cadenas/ej2.c
+++ IntroduccioToC/abril/cadenas/ej2.c
@@ -9,12 +9,17 @@ int main(){
     scanf("%s", pa1);
     puts("Introduce la palabra 2: ");
     scanf("%s", pa2);
+    //Se solicitaron las palabras y se imprimieron
     printf("\n\tAntes\n");
     printf("Palabra 1: %s", pa1);
     printf("     Palabra 2: %s\n", pa2);
-    strcpy(tmp, pa1);
-    stpcpy(pa1, pa2);
-    stpcpy(pa2, tmp);
+
+    //strcpy(<cadena_origen> en <cadena_destino>);
+    strcpy(tmp, pa1); //tmp = pa1
+    stpcpy(pa1, pa2); //pa1 = pa2
+    stpcpy(pa2, tmp); //pa2 = tmp
+
+    //Se imprimen las palabras ya intercambiadas
     printf("\n\tDespues\n");
     printf("Palabra 1: %s", pa1);
     printf("     Palabra 2: %s\n\n", pa2);
