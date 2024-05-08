f9505bbd5d2d9d7d76ce630d1ca30ac9db5856b1
itsFDavid <fdavid04@icloud.com>
2024-05-07 20:55:00 -0600

    chore: Add time measurement to ini9.c

--- IntroduccioToC/abril/punteros/ini9.c
+++ IntroduccioToC/abril/punteros/ini9.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int main(){
+    time_t inicio, fin;
+    inicio= clock();
     int i, n;
     char *buffer;
 
@@ -10,8 +13,10 @@ int main(){
     scanf("%d", &i);
 
     buffer= (char *) malloc((i+1)*sizeof(char));
-    if(buffer == NULL) exit(1);
-    
+    if(buffer == NULL) {
+        printf("Error en la asignacion de memoria\n");
+        exit(1);
+    }
     for(n=0; n<i; n++){
         buffer[n]= rand()%26+'a';
         buffer[i]='\0';
@@ -19,5 +24,7 @@ int main(){
 
     printf("Random string: %s\n", buffer);
     free(buffer);
+    fin= clock();
+    printf("Tiempo de ejecucion: %f milisegundos\n", (double)(fin-inicio)/CLOCKS_PER_SEC);
     return 0;
 }
