d378ba80e314a81eaf9b249bca50f8466969c3cc
itsFDavid <fdavid04@icloud.com>
2024-05-07 20:54:53 -0600

    feat: Add functionality to calculate and display the length of a word in ej5.c

--- IntroduccioToC/abril/cadenas/ej5.c
+++ IntroduccioToC/abril/cadenas/ej5.c
@@ -8,17 +8,18 @@ int main(){
     puts("Ingresa una palabra toda en minusculas: ");
     scanf("%s", palabra);
     fin= strlen(palabra);
+    printf("La longitud de la palabra '%s' es: %d\n", palabra, fin);
     c = palabra[0];
     ca = palabra[fin-1];
-    fin-=2;
+    printf("El primer caracter es: %c\nEl ultimo caracter es: %c\n", c, ca);
     if(c!=ca){
         printf("Tu palabra '%s' no es un palindromo\n", palabra);
         return 0;
     }else{
+        fin-=2;
         for(inicio=1; inicio <fin; inicio++, fin--){
             c=palabra[inicio];
             ca= palabra[fin];
-            //printf("[%c][%d], [%c][%d]\n", c, inicio, ca, fin);
             if(c!=ca){
                 printf("Tu palabra '%s' no es palindromo\n", palabra);
                 printf("Los caracteres que no coinden son: \n--> [ %c ]\t!=\t[ %c ] <--\n", c, ca);
