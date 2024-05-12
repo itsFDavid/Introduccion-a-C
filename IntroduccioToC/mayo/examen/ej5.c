0e3d7393abcb2710e4b15c471baeba1748ca37ca
itsFDavid <fdavid04@icloud.com>
2024-05-11 23:54:24 -0600

    modified file

--- IntroduccioToC/mayo/examen/ej5.c
+++ IntroduccioToC/mayo/examen/ej5.c
@@ -8,12 +8,13 @@ void calcularEdad(int anioNacimiento, int mesNacimiento, int diaNacimiento, int 
 
     tiempo_actual = time(NULL);
     tiempo = localtime(&tiempo_actual);
+    printf("Tiempo: %d\n", *tiempo);
+
 
     int anio_actual = tiempo->tm_year + 1900;
     int mes_actual = tiempo->tm_mon + 1;
     int dia_actual = tiempo->tm_mday;
-    printf("Fecha de nacimiento: %d/%d/%d\n", diaNacimiento, mesNacimiento, anioNacimiento);
-    printf("Fecha actual: %d/%d/%d\n", dia_actual, mes_actual, anio_actual);
+    
     // Calcular la diferencia total en días
     int total_dias_nacimiento = anioNacimiento * 365 + (anioNacimiento / 4) + mesNacimiento * 30 + diaNacimiento;
     int total_dias_actual = anio_actual * 365 + (anio_actual / 4) + mes_actual * 30 + dia_actual;
@@ -26,6 +27,8 @@ void calcularEdad(int anioNacimiento, int mesNacimiento, int diaNacimiento, int 
     *edad_anios = diferencia_anios;
     *edad_meses = diferencia_meses;
     *edad_dias = diferencia_dias;
+    printf("Fecha de nacimiento: %d/%d/%d\n", diaNacimiento, mesNacimiento, anioNacimiento);
+    printf("Fecha actual: %d/%d/%d\n", dia_actual, mes_actual, anio_actual);
 }
 
 int main() {
