01708405bfc6e3d44e91234366e111caa4242743
itsFDavid <fdavid04@icloud.com>
2024-05-11 23:39:53 -0600

    modified excersice

--- IntroduccioToC/mayo/examen/ej5.c
+++ IntroduccioToC/mayo/examen/ej5.c
@@ -1,66 +1,47 @@
 #include <stdio.h>
 #include <time.h>
 
+void calcularEdad(int anioNacimiento, int anioActual, int *edad_anios, int *edad_meses, int *edad_dias) {
+    struct tm *tiempo;
+    time_t tiempo_actual;
+    int diferencia_anios, diferencia_meses, diferencia_dias;
 
-int calcularEdad(int anio, int anioActual){
-    return anioActual-anio;
+    tiempo_actual = time(NULL);
+    tiempo = localtime(&tiempo_actual);
+
+    int anio_actual = tiempo->tm_year + 1900;
+    int mes_actual = tiempo->tm_mon + 1;
+    int dia_actual = tiempo->tm_mday;
+
+    // Calcular la diferencia total en días
+    int total_dias_nacimiento = anioNacimiento * 365 + (anioNacimiento / 4);
+    int total_dias_actual = anio_actual * 365 + (anio_actual / 4) + (mes_actual * 30) + dia_actual;
+
+    // Calcular la diferencia en años, meses y días
+    diferencia_anios = (total_dias_actual - total_dias_nacimiento) / 365;
+    diferencia_meses = ((total_dias_actual - total_dias_nacimiento) % 365) / 30;
+    diferencia_dias = ((total_dias_actual - total_dias_nacimiento) % 365) % 30;
+
+    *edad_anios = diferencia_anios;
+    *edad_meses = diferencia_meses;
+    *edad_dias = diferencia_dias;
 }
-void edadApun(){
-    time_t inicio, fin;
-    inicio= clock();
-    int anio, dia, mes;
-    puts("Ingresa tu fecha de nacimineto");
+
+int main() {
+    int anioNacimiento, edad_anios, edad_meses, edad_dias;
+    int anioActual;
+    
+    puts("Ingresa tu fecha de nacimiento");
     printf("Ingresa el año en que naciste: ");
-    scanf("%d", &anio);
-    puts("");
-    printf("Ingresa el mes en que naciste: ");
-    scanf("%d", &mes);
-    puts("");
-    printf("Ingresa el dia en que naciste: ");
-    scanf("%d", &dia);
+    scanf("%d", &anioNacimiento);
 
-    printf("Tu fecha de nacimiento es: \n\t %d/%d/%d\n", anio, mes, dia);
-
-    int anioActual, mesActual, diaActual;
-    puts("Ingresa tu fecha de actual");
+    puts("Ingresa tu fecha actual");
     printf("Ingresa el año: ");
     scanf("%d", &anioActual);
 
-    calcularEdad(anio, anioActual);
-    fin= clock();
-    printf("Tu edad es: %d\n", calcularEdad(anio, anioActual));
-    printf("El tiempo de ejecucion es: %f\n", (float)(fin-inicio)/CLOCKS_PER_SEC);
-}
-void edadSinP(){
-    time_t inicio, fin;
-    inicio= clock();
-    int anio, dia, mes;
-    puts("Ingresa tu fecha de nacimineto");
-    printf("Ingresa el año en que naciste: ");
-    scanf("%d", &anio);
-    puts("");
-    printf("Ingresa el mes en que naciste: ");
-    scanf("%d", &mes);
-    puts("");
-    printf("Ingresa el dia en que naciste: ");
-    scanf("%d", &dia);
+    calcularEdad(anioNacimiento, anioActual, &edad_anios, &edad_meses, &edad_dias);
 
-    printf("Tu fecha de nacimiento es: \n\t %d/%d/%d\n", anio, mes, dia);
+    printf("Has vivido %d años, %d meses, %d días\n", edad_anios, edad_meses, edad_dias);
 
-    int anioActual, mesActual, diaActual;
-    puts("Ingresa tu fecha de actual");
-    printf("Ingresa el año: ");
-    scanf("%d", &anioActual);
-
-    calcularEdad(anio, anioActual);
-    fin= clock();
-    printf("Tu edad es: %d\n", calcularEdad(anio, anioActual));
-    printf("El tiempo de ejecucion es: %f\n", (float)(fin-inicio)/CLOCKS_PER_SEC);
-}
-
-int main(){
-    edadApun();
-    edadSinP();
     return 0;
 }
-
